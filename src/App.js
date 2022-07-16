93ddb29c3e8dbba92e21b9299bb4a58b3f56c550
Austine Okongo <austineokongo60@gmail.com>
2022-07-16 17:23:26 +0300

    initial cart push

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <div className="App">
+     <div className='container-fluid'>
+       <nav class="navbar navbar-light bg-light">
+           <div class="container-fluid">
+              <span>Thrift Store</span>
+           </div>
+       </nav>
+
+       <div className='main'></div>
+
+       <div className='footer'>all rights reserved</div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
