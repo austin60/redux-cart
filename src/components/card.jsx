569d3f097e806e043d73bd79987fa2ce0b43d366
Austine Okongo <austineokongo60@gmail.com>
2022-07-28 15:06:28 +0300

    added animations and modal

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,22 +1,70 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade';
+import Modal from 'react-modal'
+import  Zoom  from 'react-reveal/Zoom';
+import { HiShoppingCart } from "react-icons/hi";
+
 
 class Products extends React.Component{
+  state={
+    product:null
+  }
+  openModal=(product)=>{
+    this.setState({product})
+  }
+  closeModal=()=>{
+    this.setState({product:null})
+  }
   render(){
+   const{product}=this.state;
     return(
       <div className='products'>
+        <Fade bottom cascade>
         {this.props.products.map(product=>(
           
           <div className="product" key={product._id}>
-             <div className="image">
+            <div className="image">
+             <a href={"#"+product._id} onClick={()=>this.openModal(product)}>
                <img src={product.image} alt={product.title} />
+            </a>
             </div>
+            
             <div className="product-content">
               <div className="title"> {product.title}</div>
+            
               <div className="price">${product.price}</div>
               <button className='btn btn-secondary' onClick={()=>this.props.addToCart(product)}>Add</button>
             </div>
           </div>
         ))}
+        </Fade>
+        {
+          this.state.product && 
+            <Modal isOpen={true}>
+              <Zoom >
+                  <div className='zoom'>
+                     <div className='modal-button'>
+                      <button className='btn btn-secondary'  onClick={this.closeModal}>X</button>
+                     </div>
+                     <div className='modal-data'>
+                       <div className='modal-image'>
+                         <img src={product.image} alt={product.title} />
+                       </div>
+                       <div className='modal-ps'>
+                           <p><strong> {product.title}</strong></p>
+                           <p>{product.description}</p>
+                         
+                           <p>Available :{product.availableSizes.map(x=>(
+                             <span>{" "}<button className='btn btn-secondary'>{x}</button></span>
+                             ))}</p>
+                            <button className='btn btn-warning'onClick={()=>this.props.addToCart(product)}> Add To Cart <HiShoppingCart/></button>
+                       </div>
+                    </div>
+                 </div> 
+              </Zoom> 
+            </Modal>
+          
+        }
       </div>
     )
   }
--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import { MdRemoveShoppingCart } from "react-icons/md";
+import Fade from 'react-reveal/Fade'
 
 class Cart extends React.Component{
-    
+    state={
+        showCheckout:false,
+        name:"",
+        phone:"",
+        email:""
+    }
+    handleInput=(e)=>{
+      this.setState({[e.target.name]:e.target.value})
+    }
+    createOrder=(e)=>{
+        e.preventDefault()
+        const order={
+            name:this.state.name,
+            phone:this.state.phone,
+            email:this.state.email,
+            cartItem:this.props.cartItem
+        };
+        this.props.createOrder(order);
+    }
     render(){
         return(
             <div>
@@ -16,6 +35,7 @@ class Cart extends React.Component{
           </div>
         </nav>
         <div className="cart-items">
+            <Fade left cascade>
             <ul>
                 {this.props.cartItem.map(item=>(
                     <div className="item" key={item._id}>
@@ -29,23 +49,43 @@ class Cart extends React.Component{
                         <div className="item-detail">
                             <button className="btn btn-warning" onClick={()=>this.props.removeItem(item)}><MdRemoveShoppingCart/></button>
                         </div>
+                        
                     </div>
                 ))}
             </ul>
+            </Fade>
         </div>
         {this.props.cartItem.length!==0 && (
+            <div>
             <div className="item-total">
            <b>Total:${" "}{this.props.cartItem.reduce((a,c)=>{
                 return a+(c.price*c.count)
                },0)}</b>
             <div className="proceed">
-                <button className="btn btn-warning">Proceed</button>
+                <button className="btn btn-warning" onClick={()=>{this.setState({showCheckout:true})}}>
+                    Proceed
+                </button>
             </div>
         </div>
-        )}
-         
-
+        {this.state.showCheckout && (
+            <Fade right cascade>
+            <div className="checkout-form">
+              <form className="form-control" onSubmit={this.createOrder}>
+              <label>name</label>{" "}
+                <input type="text" name="name" required onChange={this.handleInput}/><br/>
+              <label>phone </label>{" "}
+                <input type="text" name="phone" required onChange={this.handleInput}/><br/>
+              <label>e-mail </label>{" "}
+                <input type="email" name="email" required  onChange={this.handleInput}/><br/>
+              <button className="btn btn-warning" type="submit" >checkout</button>
+              </form>
             </div>
+            </Fade>
+        
+        )}
+        </div>
+        )}
+     </div>
         )
     }
 }
